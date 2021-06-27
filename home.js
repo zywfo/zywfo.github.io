e23d0d1149870a5fb44b94bf0b0dd3d64be11fd0
zywfo <iamabdulqadirforyou@gmail.com>
2021-06-27 21:36:17 +0530

    update 0.8

--- home.js
+++ home.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
-    $('audio').prop('volume',0.25);
-    
+    $('audio').prop('volume',0.4);
+    $('#mute').click(function(){
+        $('audio').prop('volume',0.0);
+    });
     $('#menubtn').click(function(){
         $("#menu").css('right','1%').css('width','99%').css('animation-play-state','running');
         $(this).hide(); 
         $('.animating>div,.logo>h2').css('animation-play-state','paused');
         $('#menubackbtn').css('display','block');
         $('#menu>ul').css('display','block');
+        $('#homepage').css('filter','grayscale(90%)');
         
     });
-    $('#menubackbtn,#homebtn').click(function(){
+    $('#menubackbtn,#homebtn,#aboutbtn').click(function(){
         $("#menu").css('right','').css('width','').css('animation-play-state','');
         $('#menubackbtn').css('display','');
         $('#menubtn').show();
         $('.animating>div,.logo>h2').css('animation-play-state','');
         $('#menu>ul').css('display','');
+        $('#homepage').css('filter','');
     });
 })
